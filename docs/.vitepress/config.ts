daab4d4bd72573e5a8c18c02f2233bc1e0b08cae
stallhoard <xhardipper@gmail.com>
2025-04-27 16:44:26 +0500

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress';
 
 export default defineConfig({
+  ignoreDeadLinks: true,
   title: 'Fastmophobia',
   description: 'The actual reliable and advanced info.',
   base: '/phasmo-wiki/',
